81324453a7e19a0b8dd846b2465ccd9fe6f2a7f6
mcgarrye <eric@eric.mcgarry.name>
2024-08-08 21:54:20 -0400

    setup env variables for backend

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateGroupDTO } from './createGroup.dto';
+import { ApiKeyGuard } from './api-key.guard';
 import { SuccessDTO } from './success.dto';
 
 @Controller()
@@ -8,6 +9,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('create')
+  @UseGuards(ApiKeyGuard)
   create(@Body() createGroupDTO: CreateGroupDTO): Promise<SuccessDTO> {
     return this.appService.create(createGroupDTO);
   }
--- server/src/callGoogleSheets.ts
+++ server/src/callGoogleSheets.ts
@@ -4,36 +4,33 @@ import { CreateGroupDTO } from './createGroup.dto';
 import { SuccessDTO } from './success.dto';
 import { InternalServerErrorException } from '@nestjs/common';
 
-// Load the credentials from the service account key file
-export const credentials = {
-  "type": "service_account",
-  "project_id": "example",
-  "private_key_id": "example",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nexample\n-----END PRIVATE KEY-----\n",
-  "client_email": "example@example.iam.gserviceaccount.com",
-  "client_id": "example",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/example.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-};
+let CLIENT: JWT;
+let SHEETS: any;
 
-// Create a new JWT client using the credentials
-const client = new JWT({
-    email: credentials.client_email,
-    key: credentials.private_key,
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-});
+function initialize() {
+    if (!CLIENT){
+        // Create a new JWT client using the credentials
+        CLIENT = new JWT({
+            email: process.env.CLIENT_EMAIL,
+            key: process.env.PRIVATE_KEY,
+            scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+        });
+    }
 
-// Authorize and create a Google Sheets API instance
-const sheets = google.sheets({ version: "v4", auth: client });
+    if (!SHEETS){
+        // Authorize and create a Google Sheets API instance
+        SHEETS = google.sheets({ version: "v4", auth: CLIENT });
+    }
+}
 
 export async function append(data: CreateGroupDTO): Promise<SuccessDTO> {
     try {
+        initialize()
+        
         // Your spreadsheet ID
+        const spreadsheetId = process.env.SPREADSHEET_ID;
 
-        const postGroupData =  await sheets.spreadsheets.values.append({
+        const postGroupData =  await SHEETS.spreadsheets.values.append({
             spreadsheetId: spreadsheetId,
             range: "Group Data",
             valueInputOption: "RAW",
@@ -67,7 +64,7 @@ export async function append(data: CreateGroupDTO): Promise<SuccessDTO> {
             member.headOfGroup
         ])
 
-        const postGroupMembers =  await sheets.spreadsheets.values.append({
+        const postGroupMembers =  await SHEETS.spreadsheets.values.append({
             spreadsheetId: spreadsheetId,
             range: "Group Members",
             valueInputOption: "RAW",
